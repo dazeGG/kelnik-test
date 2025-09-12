5a73f15dd19a2a762335851950fe0a116823a62c
VladimirBelyaev <vogisa@mail.ru>
2025-09-12 17:38:48 +0300

    feat: some base ui button view control

--- app/components/base/ui-button.vue
+++ app/components/base/ui-button.vue
@@ -4,6 +4,7 @@ const props = withDefaults(
 		type?: 'default' | 'primary'
 		circle?: boolean
 		disabled?: boolean
+		borderless?: boolean
 	}>(),
 	{
 		type: 'default',
@@ -12,24 +13,59 @@ const props = withDefaults(
 </script>
 
 <template>
-	<button class="button" :disabled="props.disabled" :class="[`button--${props.type}`]">
+	<button
+		class="ui-button"
+		:data-circle="props.circle ? '' : undefined"
+		:data-borderless="props.borderless ? '' : undefined"
+		:disabled="props.disabled"
+		:class="[`ui-button--${props.type}`]"
+	>
 		<slot />
 	</button>
 </template>
 
-<style scoped lang="scss">
-.button {
-	outline: 1px solid var(--color-button-border);
-	padding: 8px 24px;
-	border-radius: 25px;
-	transition: 100ms outline-color ease-in-out;
+<style lang="scss">
+.ui-button {
+	transition-timing-function: ease-in-out;
+	transition-duration: 100ms;
+	transition-property: outline-color, box-shadow;
 
-	&:hover {
-		outline-color: var(--color-accent);
+	&.ui-button--default {
+		background-color: white;
+		--outline-color: var(--color-button-border);
+	}
+
+	&.ui-button--primary {
+		background-color: var(--color-accent);
+		color: white;
+		--outline-color: var(--color-accent);
+	}
+
+	&:not([data-borderless]) {
+		outline: 1px solid var(--outline-color);
+
+		&:hover {
+			outline-color: var(--color-accent);
+		}
+	}
+
+	&[data-circle] {
+		width: 44px;
+		height: 44px;
+		border-radius: 50%;
+	}
+
+	&:not([data-circle]) {
+		padding: 8px 24px;
+		border-radius: 25px;
 	}
 
 	&[disabled] {
 		pointer-events: none;
+	}
+
+	&:hover {
+		box-shadow: 0 6px 20px 0 #95D0A1;
 	}
 }
 </style>
