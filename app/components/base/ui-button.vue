d4d118e22531a8969016558b957da035dedb2297
VladimirBelyaev <vogisa@mail.ru>
2025-09-12 17:26:29 +0300

    feat: init base ui button

---
+++ app/components/base/ui-button.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = withDefaults(
+	defineProps<{
+		type?: 'default' | 'primary'
+		circle?: boolean
+		disabled?: boolean
+	}>(),
+	{
+		type: 'default',
+	},
+);
+</script>
+
+<template>
+	<button class="button" :disabled="props.disabled" :class="[`button--${props.type}`]">
+		<slot />
+	</button>
+</template>
+
+<style scoped lang="scss">
+.button {
+	outline: 1px solid var(--color-button-border);
+	padding: 8px 24px;
+	border-radius: 25px;
+	transition: 100ms outline-color ease-in-out;
+
+	&:hover {
+		outline-color: var(--color-accent);
+	}
+
+	&[disabled] {
+		pointer-events: none;
+	}
+}
+</style>
--- app/components/flats/list/flats-list.vue
+++ app/components/flats/list/flats-list.vue
@@ -2,18 +2,31 @@
 import { onBeforeMount } from "vue";
 import { useFlatsStore } from "../../../stores";
 
+import UIButton from "../../base/ui-button.vue";
 import FlatsTable from './flats-table.vue';
 
 const flatsStore = useFlatsStore();
 
 onBeforeMount(() => flatsStore.loadFlats({}));
+
+const loadMore = () => {
+	flatsStore.loadFlats({ offset: flatsStore.flats.length });
+};
 </script>
 
 <template>
 	<div class="flats-list">
 		<FlatsTable :flats="flatsStore.flats" />
+
+		<UIButton :disabled="!flatsStore.hasMore" @click="loadMore">Загрузить еще</UIButton>
 	</div>
 </template>
 
 <style scoped lang="scss">
+.flats-list {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 48px;
+}
 </style>
