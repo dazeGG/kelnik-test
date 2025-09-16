a8a063c3d6ae518955e1c49e322a88e2b7539f12
VladimirBelyaev <vogisa@mail.ru>
2025-09-16 15:08:16 +0300

    feat: created ui slider component

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import 'vue-slider-component/dist-css/vue-slider-component.css';
+import 'vue-slider-component/theme/default.css';
+</script>
+
 <template>
 	<div>
 		<Flats />
