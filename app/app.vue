2bf9a2afee8f1f09bf8cef4c9e6fdec41b357c5b
Vladimir <86754494+dazeGG@users.noreply.github.com>
2025-09-19 14:54:22 +0300

    feature/up-button (#8)
    * chore: added arrow up icon
    
    * feat: created up button
    
    * chore: components order

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import 'vue-slider-component/dist-css/vue-slider-component.css';
 import 'vue-slider-component/theme/default.css';
+
+import UpButton from "@/components/e2e/up-button.vue";
 </script>
 
 <template>
 	<div>
+		<UpButton />
+
 		<Flats />
 	</div>
 </template>
---
+++ app/components/e2e/up-button.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useWindowScroll } from '@vueuse/core';
+
+import UIButton from "@/components/base/ui-button.vue";
+import ArrowUp from '@/assets/icons/arrow-up.svg';
+
+const { y } = useWindowScroll();
+
+const scrollToTop = () => {
+	window.scrollTo(0, 0);
+};
+</script>
+
+<template>
+	<UIButton v-show="y > 0" circle type="primary" class="up-button" @click="scrollToTop">
+		<ArrowUp />
+	</UIButton>
+</template>
+
+<style scoped lang="scss">
+.up-button {
+	position: fixed;
+	bottom: 32px;
+	right: 32px;
+
+	width: 40px;
+	height: 40px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
